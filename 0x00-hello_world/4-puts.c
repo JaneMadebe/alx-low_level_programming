befb752617300bfc5ab98818d4a28bbee72ae3ac
JaneMadebe <Janemadebe@gmail.com>
2023-02-18 06:53:23 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
- * main - prints "progranning that prints line with puts element"
+ * main - A program that prints a line with puts function
  * Return: 0
  */
 int main(void)
 {
-puts("\"programming is like building a multilingual puzzle")
-return (0)
+puts("\"Programming is like building a multilingual puzzle")
+return (0);
 }
+
