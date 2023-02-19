0b3a377a59ade1234bde48e61fec457df5fac6ea
JaneMadebe <Janemadebe@gmail.com>
2023-02-18 23:03:14 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,5 +6,5 @@
  */
 int main(void)
 {
-fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19n\n", stdout);
+fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19n\n", stdout);
 return (1);
