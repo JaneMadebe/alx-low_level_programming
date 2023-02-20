24b8ce6f4120522977704d527ae92b5ded34ebc1
JaneMadebe <Janemadebe@gmail.com>
2023-02-20 10:21:11 -0800

    0-positive_or_negative.c

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+n = random.randint(-10, 10)  # assigns a random integer between -10 and 10 to n
+
+if n > 0:
+    print("n is positive")
+elif n == 0:
+    print("n is zero")
+else:
+    print("n is negative")
+
