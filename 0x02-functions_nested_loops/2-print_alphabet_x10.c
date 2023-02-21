c95e6a86e4cfe37bdabfc1710a83557092da3faa
JaneMadebe <Janemadebe@gmail.com>
2023-02-21 11:43:48 -0800

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,19 +4,18 @@
  * Return: void
  */
 
-void print_alphabet_x10(void)
-{
-	char c;
-	int i = 0;
-
-	while (i <= 0)
-	{
-	for (c = 'a' ; c < 'z'; c++)
-	{
-_putchar(c);
-	}
-	_putchar('\n');
-	i++;
+void print_alphabet_x10(void) {
+   
+    char letter = 'a';
+    int i, j;
+    for (i = 1; i <= 10; i++) {
+        for (j = 0; j < 26; j++) {
+            _putchar(letter);
+            letter++;
+        }
+        letter = 'a'; // reset letter to 'a' for next line
+        _putchar('\n');
+    }
 }
 }
 
