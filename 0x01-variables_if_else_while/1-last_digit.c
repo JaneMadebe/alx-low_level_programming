ef0276caf6e56c54e672b87fa32c060f8ac82fa3
JaneMadebe <Janemadebe@gmail.com>
2023-02-20 20:10:15 -0800

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -11,7 +11,7 @@ int main(void)
 {
 int n;
 srand(time(0));
-n- rand() - RAND_MAX / 2;
+"n"- rand() - RAND_MAX / 2;
 if ((n % 10) > 5)
 {
 printf("Last digit of %d is %d and is greater then 5\n" ,n , n% 10);
@@ -22,7 +22,7 @@ printf("Last digit of %d is %d ad is less than 6 and not 0\n", n, n % 10);
 }
 else
 {
-prinft("Last digit of %d is %d and is 0\n" , n , n %10);
+printf("Last digit of %d is %d and is 0\n" , n , n %10);
 }
 return (0);
 }
