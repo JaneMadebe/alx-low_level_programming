fc0f9fa4022079d28d8c8b421ea132eb6c257ee0
JaneMadebe <Janemadebe@gmail.com>
2023-02-22 11:59:49 -0800

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,7 +8,7 @@
 void jack_bauer(void)
 
 {
- int a, b, c, d;
+int a, b, c, d;
 
 for (a = 0; a <= 2; a++)
 {
@@ -23,8 +23,8 @@ for (d = 0; d <= 9; d++)
 _putchar(a + '0');
 _putchar(b + '0');
 _putchar(58);
-_putchar( c + '0');
-_putchar( d + '0');
+_putchar(c + '0');
+_putchar(d + '0');
 _putchar('\n');
 }
 }
