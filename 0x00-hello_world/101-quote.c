dc41a7aa946c2cb1a0d87fd6a31ed35df480c05d
JaneMadebe <Janemadebe@gmail.com>
2023-02-25 01:58:24 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
- * main - A C program that prints a line to the standard error
+ * main - A c program that prints a line to the standard error
  * Return: 1 (success)
  */
-int main()
+
+int main(void)
+
 {
-fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19n\n");
-return 1;
+fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+retun (1);
