f8060751d3357986692adef439305f297b39d1e7
JaneMadebe <Janemadebe@gmail.com>
2023-02-27 19:47:59 -0800

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string, in reverse
+ * @s: string
+ * Return: 0 (success)
+ */
+
+void print_rev(char *s)
+{
+int longi = 0;
+int o;
+
+while (*s != '\0')
+{
+longi++;
+s++;
+}
+s--;
+for (o = longi; o > 0; 0--)
+{
+_putchar(*s);
+s--;
+}
+_putchar('\n');
+return (0);
+}
