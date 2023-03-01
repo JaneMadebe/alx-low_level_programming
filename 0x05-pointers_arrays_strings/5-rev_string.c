3a378f251915f18ee1f07ecd83b34cb90f6a73a1
JaneMadebe <Janemadebe@gmail.com>
2023-02-28 19:59:56 -0800

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,17 +8,16 @@
 
 void rev_string(char *s)
 {
-char rev - s[0];
-int counter = o;
-int i;
+	int len = 0, index = 0;
+	char tmp;
 
-while (s[counter] != '\0')
-counter++;
-for (i = 0; i < counter; i++)
-{
-counter--;
-rev - s[i];
-s[i] = s[counter];
-s[counter] = rev;
-}
+	while (s[index++])
+	len++;
+
+	for (index = len - 1; index >= len / 2; index--)
+	{
+	tmp = s[index];
+	s[index] = s[len - index - 1];
+	s[len - index - 1] = tmp;
+	}
 }
