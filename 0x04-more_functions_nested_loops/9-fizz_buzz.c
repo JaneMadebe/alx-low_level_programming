24f392d6b39f55dc413ebdf9c4cd57ca13b05a0c
JaneMadebe <Janemadebe@gmail.com>
2023-03-01 06:13:54 -0800

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,19 +15,19 @@ for (x = 1; x <= 100; x++)
 {
 if ((x % 3 == 0) && (x % 5 == 0))
 {
-printf("fizzBuzz");
+printf("fizzBuzz ");
 }
 else if (x % 3 == 0)
 {
-printf("fizz");
+printf("fizz ");
 }
 else if (x % 5 == 0)
 {
-printf("Buzz");
+printf("Buzz ");
 }
 else
 {
-printf("%d", x);
+printf("%d ", x);
 }
 if (x != 100)
 {
